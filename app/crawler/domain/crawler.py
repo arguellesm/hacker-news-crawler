33930e2b5cb528dd6211703b7ad41d49d7dd0974
arguellesm <arguellesesc@gmail.com>
2023-03-15 18:46:22 +0100

    add crawler.py

---
+++ app/crawler/domain/crawler.py
@@ -0,0 +1,9 @@
+class Crawler:
+    """
+    Crawler interface.
+    """
+
+    def get_entries(self,num_entries=30):
+        """Retrieves a number of entries."""
+
+        raise NotImplementedError
