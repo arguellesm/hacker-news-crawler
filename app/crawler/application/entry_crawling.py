32292122ba57ef90b29462d9a16e5a903dcde018
arguellesm <arguellesesc@gmail.com>
2023-03-16 10:49:02 +0100

    add entry_crawling.py

---
+++ app/crawler/application/entry_crawling.py
@@ -0,0 +1,22 @@
+from app.crawler.domain.crawler import Crawler
+from app.crawler.domain.entry import Entry
+
+
+class EntryCrawling:
+    """
+    Represents an entry crawling process.
+    """
+
+    def __init__(self, crawler, repository):
+        """Init EntryCrawling with a crawler and a repository."""
+
+        self.crawler = crawler
+        self.repository = repository
+
+    
+    def crawl(self):
+        """Crawls entries."""
+        
+        entries = self.crawler.get_entries()
+        self.repository.save_entries(entries)
+        
